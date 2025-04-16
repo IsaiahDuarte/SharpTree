c8c5f3ca2ee0ffc0a7ac1baaf2a210511af06800
Isaiah Duarte <isaiah.duarte@controlup.com>
2025-04-16 10:24:25 -0400

    Adjusted logic of FileSystemReader to calculate proper size with minsize

--- SharpTree.Core.Powershell/Services/FileSystemReader.cs
+++ SharpTree.Core.Powershell/Services/FileSystemReader.cs
@@ -1,8 +1,9 @@
 ﻿using SharpTree.Core.Models;
-using System.Collections.Generic;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SharpTree.Core.Services
@@ -15,9 +16,8 @@ namespace SharpTree.Core.Services
                 throw new ArgumentException($"Path {path} does not exist", nameof(path));
 
             var directoryInfo = new DirectoryInfo(path);
-            var node = new DirectoryNode(directoryInfo.Name);
+            var processingNode = new DirectoryNode(directoryInfo.Name);
             long totalSize = 0;
-
             FileSystemInfo[] entries;
             try
             {
@@ -26,39 +26,29 @@ namespace SharpTree.Core.Services
             catch (Exception ex)
             {
                 if (verbose)
-                    Console.Error.WriteLine($"Error reading {path}: {ex.Message}");
-                node.Size = 0;
-                return node;
+                    Console.Error.WriteLine($"Error reading root {path}: {ex.Message}");
+                return new RootNode(directoryInfo.Name, 0, new List<INode>());
             }
 
             var syncObj = new object();
-
             Parallel.ForEach(entries, entry =>
             {
-                ProcessEntry(entry, node, minSize, maxDepth, verbose, ref totalSize, syncObj, 0);
+                ProcessEntry(entry, processingNode, minSize, maxDepth, verbose, ref totalSize, syncObj, 0);
             });
 
-            node.Size = totalSize;
-            node.SortChildren();
-
-            if (node.IsDirectory)
-            {
-                if (verbose)
-                    Console.WriteLine($"Returning RootNode: {node.Name}");
-                return new RootNode(node.Name, totalSize, node.Children);
-            }
-
+            processingNode.Size = totalSize;
+            processingNode.SortChildren();
             if (verbose)
-                Console.WriteLine($"Returning DirectoryNode: {node.Name}");
-            return node;
+                Console.WriteLine($"Processed Root: {processingNode.Name} with Size: {processingNode.Size}");
+
+            return new RootNode(processingNode.Name, processingNode.Size, processingNode.Children);
         }
 
-        private static INode ReadRecursive(string path, long minSize, int maxDepth, bool isRoot, int currentDepth, bool verbose)
+        private static DirectoryNode ReadRecursive(string path, long minSize, int maxDepth, int currentDepth, bool verbose)
         {
             var directoryInfo = new DirectoryInfo(path);
             var node = new DirectoryNode(directoryInfo.Name);
             long totalSize = 0;
-
             FileSystemInfo[] entries;
             try
             {
@@ -79,20 +69,19 @@ namespace SharpTree.Core.Services
 
             node.Size = totalSize;
             node.SortChildren();
-
-            if (isRoot && node.IsDirectory)
-            {
-                if (verbose)
-                    Console.WriteLine($"Returning RootNode: {node.Name}");
-                return new RootNode(node.Name, totalSize, node.Children);
-            }
-
             if (verbose)
-                Console.WriteLine($"Returning DirectoryNode: {node.Name}");
+                Console.WriteLine($"Returning DirectoryNode: {node.Name} with Size: {node.Size}");
             return node;
         }
 
-        private static void ProcessEntry(FileSystemInfo entry, DirectoryNode node, long minSize, int maxDepth, bool verbose, ref long totalSize, object syncObj, int currentDepth)
+        private static void ProcessEntry(FileSystemInfo entry,
+                                         DirectoryNode node,
+                                         long minSize,
+                                         int maxDepth,
+                                         bool verbose,
+                                         ref long totalSize,
+                                         object syncObj,
+                                         int currentDepth)
         {
             try
             {
@@ -107,55 +96,91 @@ namespace SharpTree.Core.Services
                 {
                     if (verbose)
                         Console.WriteLine($"Processing file: {fileInfo.FullName}");
-                    if (fileInfo.Length >= minSize)
+
+                    long currentFileSize = 0;
+                    try
                     {
-                        if (syncObj != null)
+                        currentFileSize = fileInfo.Length;
+                    }
+                    catch (FileNotFoundException fnfEx)
+                    {
+                        if (verbose)
+                            Console.Error.WriteLine($"Error accessing file {fileInfo.FullName}: {fnfEx.Message}");
+                        return;
+                    }
+                    catch (IOException ioEx)
+                    {
+                        if (verbose)
+                            Console.Error.WriteLine($"IO Error accessing file {fileInfo.FullName}: {ioEx.Message}");
+                        return;
+                    }
+
+                    if (syncObj != null)
+                    {
+                        lock (syncObj)
                         {
-                            lock (syncObj)
-                            {
-                                totalSize += fileInfo.Length;
-                                node.AddChild(new FileNode(fileInfo.Name, fileInfo.Length));
-                            }
+                            totalSize += currentFileSize;
+                            if (currentFileSize >= minSize)
+                                node.AddChild(new FileNode(fileInfo.Name, currentFileSize));
                         }
-                        else
-                        {
-                            totalSize += fileInfo.Length;
-                            node.AddChild(new FileNode(fileInfo.Name, fileInfo.Length));
-                        }
+                    }
+                    else
+                    {
+                        totalSize += currentFileSize;
+                        if (currentFileSize >= minSize)
+                            node.AddChild(new FileNode(fileInfo.Name, currentFileSize));
                     }
                 }
                 else if (entry is DirectoryInfo dirInfo)
                 {
                     if (verbose)
                         Console.WriteLine($"Processing directory: {dirInfo.FullName}");
+
                     if (maxDepth == -1 || currentDepth < maxDepth)
                     {
-                        var childDir = ReadRecursive(
-                            dirInfo.FullName,
-                            minSize,
-                            maxDepth,
-                            isRoot: false,
-                            currentDepth: currentDepth + 1,
-                            verbose);
-
-                        if (childDir.Size > 0)
+                        var childDirNode = ReadRecursive(dirInfo.FullName, minSize, maxDepth, currentDepth + 1, verbose);
+                        long childDirSize = childDirNode.Size;
+                        if (childDirSize > 0 || (childDirNode.Children != null && childDirNode.Children.Any()))
                         {
                             if (syncObj != null)
                             {
                                 lock (syncObj)
                                 {
-                                    totalSize += childDir.Size;
-                                    node.AddChild(childDir);
+                                    totalSize += childDirSize;
+                                    node.AddChild(childDirNode);
                                 }
                             }
                             else
                             {
-                                totalSize += childDir.Size;
-                                node.AddChild(childDir);
+                                totalSize += childDirSize;
+                                node.AddChild(childDirNode);
+                            }
+                        }
+                        else
+                        {
+                            if (syncObj != null)
+                            {
+                                lock (syncObj)
+                                {
+                                    totalSize += childDirSize;
+                                }
+                            }
+                            else
+                            {
+                                totalSize += childDirSize;
                             }
                         }
                     }
+                    else if (verbose)
+                    {
+                        Console.WriteLine($"Skipping directory due to max depth: {dirInfo.FullName}");
+                    }
                 }
+            }
+            catch (UnauthorizedAccessException uaEx)
+            {
+                if (verbose)
+                    Console.Error.WriteLine($"Access denied for {entry.FullName}: {uaEx.Message}");
             }
             catch (Exception ex)
             {
--- SharpTree.Core/Services/FileSystemReader.cs
+++ SharpTree.Core/Services/FileSystemReader.cs
@@ -1,7 +1,11 @@
 ﻿using SharpTree.Core.Models;
+using System.Collections.Generic;
+using System;
 using System.Diagnostics;
 using System.IO;
-using System.Reflection;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SharpTree.Core.Services
 {
@@ -13,70 +17,47 @@ namespace SharpTree.Core.Services
                 throw new ArgumentException($"Path {path} does not exist", nameof(path));
 
             var directoryInfo = new DirectoryInfo(path);
-            var node = new DirectoryNode(directoryInfo.Name);
+            var processingNode = new DirectoryNode(directoryInfo.Name);
             long totalSize = 0;
 
-            var enumerationOptions = new EnumerationOptions
-            {
-                IgnoreInaccessible = true,
-                RecurseSubdirectories = false,
-                AttributesToSkip = FileAttributes.ReparsePoint,
-                ReturnSpecialDirectories = false
-            };
-
-            IEnumerable<FileSystemInfo> entries;
+            FileSystemInfo[] entries;
             try
             {
-                entries = directoryInfo.EnumerateFileSystemInfos("*", enumerationOptions);
+                entries = directoryInfo.GetFileSystemInfos();
             }
             catch (Exception ex)
             {
                 if (verbose)
-                    Console.Error.WriteLine($"Error reading {path}: {ex.Message}");
-                node.Size = 0;
-                return node;
+                    Console.Error.WriteLine($"Error reading root {path}: {ex.Message}");
+                return new RootNode(directoryInfo.Name, 0, []);
             }
 
             var syncObj = new object();
 
             Parallel.ForEach(entries, entry =>
             {
-                ProcessEntry(entry, node, minSize, maxDepth, verbose, ref totalSize, syncObj, 0);
+                ProcessEntry(entry, processingNode, minSize, maxDepth, verbose, ref totalSize, syncObj, 0); // FIX CS1503: Pass processingNode
             });
 
-            node.Size = totalSize;
-            node.SortChildren();
-
-            if (node.IsDirectory)
-            {
-                if (verbose)
-                    Console.WriteLine($"Returning RootNode: {node.Name}");
-                return new RootNode(node.Name, totalSize, node.Children);
-            }
+            processingNode.Size = totalSize;
+            processingNode.SortChildren();
 
             if (verbose)
-                Console.WriteLine($"Returning DirectoryNode: {node.Name}");
-            return node;
+                Console.WriteLine($"Processed Root: {processingNode.Name} with Size: {processingNode.Size}");
+
+            return new RootNode(processingNode.Name, processingNode.Size, processingNode.Children);
         }
 
-        private static INode ReadRecursive(string path, long minSize, int maxDepth, bool isRoot, int currentDepth, bool verbose)
+        private static DirectoryNode ReadRecursive(string path, long minSize, int maxDepth, int currentDepth, bool verbose)
         {
             var directoryInfo = new DirectoryInfo(path);
             var node = new DirectoryNode(directoryInfo.Name);
             long totalSize = 0;
 
-            var enumerationOptions = new EnumerationOptions
-            {
-                IgnoreInaccessible = true,
-                RecurseSubdirectories = false,
-                AttributesToSkip = FileAttributes.ReparsePoint,
-                ReturnSpecialDirectories = false
-            };
-
-            IEnumerable<FileSystemInfo> entries;
+            FileSystemInfo[] entries;
             try
             {
-                entries = directoryInfo.EnumerateFileSystemInfos("*", enumerationOptions);
+                entries = directoryInfo.GetFileSystemInfos();
             }
             catch (Exception ex)
             {
@@ -94,40 +75,64 @@ namespace SharpTree.Core.Services
             node.Size = totalSize;
             node.SortChildren();
 
-            if (isRoot && node.IsDirectory)
-            {
-                if (verbose)
-                    Console.WriteLine($"Returning RootNode: {node.Name}");
-                return new RootNode(node.Name, totalSize, node.Children);
-            }
-
             if (verbose)
-                Console.WriteLine($"Returning DirectoryNode: {node.Name}");
+                Console.WriteLine($"Returning DirectoryNode: {node.Name} with Size: {node.Size}");
+
             return node;
         }
 
-        private static void ProcessEntry(FileSystemInfo entry, DirectoryNode node, long minSize, int maxDepth, bool verbose, ref long totalSize, object syncObj, int currentDepth)
+
+        private static void ProcessEntry(FileSystemInfo entry, DirectoryNode node, long minSize, int maxDepth, bool verbose, ref long totalSize, object? syncObj, int currentDepth)
         {
             try
             {
+                if ((entry.Attributes & FileAttributes.ReparsePoint) != 0)
+                {
+                    if (verbose)
+                        Console.WriteLine($"Skipping reparse point: {entry.FullName}");
+                    return;
+                }
+
                 if (entry is FileInfo fileInfo)
                 {
                     if (verbose)
                         Console.WriteLine($"Processing file: {fileInfo.FullName}");
-                    if (fileInfo.Length >= minSize)
+
+                    long currentFileSize = 0;
+                    try
                     {
-                        if (syncObj != null)
+                       currentFileSize = fileInfo.Length;
+                    }
+                    catch (FileNotFoundException fnfEx)
+                    {
+                       if (verbose)
+                          Console.Error.WriteLine($"Error accessing file {fileInfo.FullName}: {fnfEx.Message}");
+                       return;
+                    }
+                    catch (IOException ioEx)
+                    {
+                       if (verbose)
+                          Console.Error.WriteLine($"IO Error accessing file {fileInfo.FullName}: {ioEx.Message}");
+                       return;
+                    }
+
+                    if (syncObj is not null)
+                    {
+                        lock (syncObj)
                         {
-                            lock (syncObj)
+                            totalSize += currentFileSize;
+                            if (currentFileSize >= minSize)
                             {
-                                totalSize += fileInfo.Length;
-                                node.AddChild(new FileNode(fileInfo.Name, fileInfo.Length));
+                                node.AddChild(new FileNode(fileInfo.Name, currentFileSize));
                             }
                         }
-                        else
+                    }
+                    else
+                    {
+                        totalSize += currentFileSize;
+                        if (currentFileSize >= minSize)
                         {
-                            totalSize += fileInfo.Length;
-                            node.AddChild(new FileNode(fileInfo.Name, fileInfo.Length));
+                             node.AddChild(new FileNode(fileInfo.Name, currentFileSize));
                         }
                     }
                 }
@@ -135,34 +140,57 @@ namespace SharpTree.Core.Services
                 {
                     if (verbose)
                         Console.WriteLine($"Processing directory: {dirInfo.FullName}");
+
                     if (maxDepth == -1 || currentDepth < maxDepth)
                     {
-                        var childDir = ReadRecursive(
+                        var childDirNode = ReadRecursive(
                             dirInfo.FullName,
                             minSize,
                             maxDepth,
-                            isRoot: false,
-                            currentDepth: currentDepth + 1,
+                            currentDepth + 1,
                             verbose);
 
-                        if (childDir.Size > 0)
+                        long childDirSize = childDirNode.Size;
+
+                        if (childDirSize > 0 || childDirNode.Children.Any())
                         {
-                            if (syncObj != null)
-                            {
-                                lock (syncObj)
-                                {
-                                    totalSize += childDir.Size;
-                                    node.AddChild(childDir);
-                                }
-                            }
-                            else
-                            {
-                                totalSize += childDir.Size;
-                                node.AddChild(childDir);
-                            }
+                             if (syncObj is not null)
+                             {
+                                 lock (syncObj)
+                                 {
+                                     totalSize += childDirSize;
+                                     node.AddChild(childDirNode);
+                                 }
+                             }
+                             else
+                             {
+                                 totalSize += childDirSize;
+                                 node.AddChild(childDirNode);
+                             }
+                        } else {
+                             if (syncObj is not null)
+                             {
+                                 lock (syncObj)
+                                 {
+                                     totalSize += childDirSize;
+                                 }
+                             }
+                             else
+                             {
+                                 totalSize += childDirSize;
+                             }
                         }
                     }
+                    else if (verbose)
+                    {
+                         Console.WriteLine($"Skipping directory due to max depth: {dirInfo.FullName}");
+                    }
                 }
+            }
+            catch (UnauthorizedAccessException uaEx)
+            {
+                if (verbose)
+                    Console.Error.WriteLine($"Access denied for {entry.FullName}: {uaEx.Message}");
             }
             catch (Exception ex)
             {
